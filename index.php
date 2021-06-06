93d8270906dc8f51f9eea2e71289d58d3891ddf4
Madushan Gunarathne <madushangunarathne@gmail.com>
2021-06-06 10:28:33 +0530

    Setup Database connection and other resources

---
+++ index.php
@@ -0,0 +1,11 @@
+<!-- Including Header -->
+
+<?php include_once 'resources/header.php'; ?>
+
+
+
+
+
+
+<!-- Including Footer -->
+<?php include_once 'resources/footer.php'; ?>
---
+++ resources/header.php
@@ -0,0 +1,17 @@
+<!-- Including DB Connection -->
+<?php include_once 'resources/db.php'; ?>
+
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8" />
+    <meta http-equiv="X-UA-Compatible" content="IE=edge" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <title>Home | Leafy Green Oragnic Food Store</title>
+    <!-- CSS only -->
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-+0n0xVW2eSR5OomGNYDnhzAbDsOXxcvSN1TPprVMTNDbiYZCxYbOOl7+AMvyTG2x" crossorigin="anonymous">
+    <link rel="stylesheet" href="styles/styles.css">
+</head>
+
+<body>
