904a0389e9c32f7ae59533927df39386e5324a59
optimuspnj <contact@prabhashlk.com>
2021-07-01 22:18:08 +0530

    Add: Ajax script for registration and minor fixes

--- login.php
+++ login.php
@@ -22,10 +22,10 @@
                 </div>
                 <div class="form-group">
                     <!-- Changed attribute type for prevent refresh, added onclick function and a spinner for wait until server response -->
-                    <button type="button" name="login" class="btn btn-success px-4" onclick="ajaxCall()">Login <span id="save-btn-spinner" class="spinner-border-sm"></span></button>
+                    <button type="button" name="login" class="btn btn-success px-4" onclick="loginAjaxCall()">Login <span id="login-btn-spinner" class="spinner-border-sm"></span></button>
                 </div>
+                <!-- Added div to display messages -->
                 <div id="loginStatusArea"></div>
-                
             </form>
 
 
@@ -38,9 +38,9 @@
 
                 </div>
                 <div class="form-group">
-                    <label for="email">Email address</label>
-                    <input type="email" class="form-control" id="email" placeholder="Enter email">
-
+                    <label for="regemail">Email address</label>
+                    <!-- Changed id to avoid conflicts with login form -->
+                    <input type="email" class="form-control" id="regemail" placeholder="Enter email">
                 </div>
                 <div class="form-group">
                     <label for="phone">Phone Number</label>
@@ -49,8 +49,9 @@
                 </div>
 
                 <div class="form-group">
-                    <label for="password">Password</label>
-                    <input type="password" class="form-control" id="password" placeholder="Password">
+                    <label for="regpassword">Password</label>
+                    <!-- Changed id to avoid conflicts with login form -->
+                    <input type="password" class="form-control" id="regpassword" placeholder="Password">
                 </div>
                 <div class="form-group">
                     <label for="usertype">User Type</label><br>
@@ -66,8 +67,11 @@
 
                 </div>
                 <div class="form-group">
-                    <button type="submit" name="register" class="btn btn-success px-4">Register</button>
+                    <!-- Changed attribute type for prevent refresh, added onclick function and a spinner for wait until server response -->
+                    <button type="button" name="register" class="btn btn-success px-4" onclick="regAjaxCall()">Register <span id="register-btn-spinner" class="spinner-border-sm"></span></button>
                 </div>
+                <!-- Added div to display messages -->
+                <div id="regStatusArea"></div>
             </form>
 
         </div>
@@ -94,16 +98,16 @@
     }
 
     //Ajax script for submit login details, display errors and success redirects
-    function ajaxCall() {
+    function loginAjaxCall() {
         var val1 = $('#email').val();
         var val2 = $('#password').val();
-        $("#save-btn-spinner").addClass("spinner-border");
+        $("#login-btn-spinner").addClass("spinner-border");
         $.ajax({
             type: 'POST',
             url: './login_process.php',
             data: { email: val1, password: val2 },
             success: function(response) {
-                $("#save-btn-spinner").removeClass("spinner-border");
+                $("#login-btn-spinner").removeClass("spinner-border");
                 if ($.trim(response) == "username") {
                     $('#loginStatusArea').html("<div class='alert alert-danger'><strong>Error:</strong> Username is incorrect!</div>");
                 }
@@ -124,6 +128,45 @@
                 }
                 else {
                     $('#loginStatusArea').html("<div class='alert alert-danger'><strong>Error:</strong> Invalid server respose!</div>");
+                }
+            }
+        });
+    }
+
+    //Ajax script for submit registration details, display errors and success redirects
+    function regAjaxCall() {
+        var val1 = $('#name').val();
+        var val2 = $('#regemail').val();
+        var val3 = $('#phone').val();
+        var val4 = $('#regpassword').val();
+        var val5 = $('#usertype').val();
+        $("#register-btn-spinner").addClass("spinner-border");
+        $.ajax({
+            type: 'POST',
+            url: './register_process.php',
+            data: { name: val1, regemail: val2, phone: val3, regpassword: val4, usertype: val5 },
+            success: function(response) {
+                $("#register-btn-spinner").removeClass("spinner-border");
+                if ($.trim(response) == "email") {
+                    $('#regStatusArea').html("<div class='alert alert-danger'><strong>Error:</strong> Email exists!</div>");
+                }
+                else if ($.trim(response) == "phone") {
+                    $('#regStatusArea').html("<div class='alert alert-danger'><strong>Error:</strong> Phone number exists!</div>");
+                }
+                else if ($.trim(response) == "regerror") {
+                    $('#regStatusArea').html("<div class='alert alert-danger'><strong>Error:</strong> Registration error!</div>");
+                }
+                else if ($.trim(response) == "regsuccess") {
+                    $('#regStatusArea').html("<div class='alert alert-success'><strong>Success: </strong> Registration successful!</div>");
+                    if (val5 == "Customer") {
+                        window.location.href = "./buyer.php";
+                    }
+                    else if (val5 == "Seller") {
+                        window.location.href = "./seller.php";
+                    }
+                }
+                else {
+                    $('#regStatusArea').html("<div class='alert alert-danger'><strong>Error:</strong> Unknown server response</div>");
                 }
             }
         });
