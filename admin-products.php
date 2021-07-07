110aecec68e74f2e48b434f68b43c99e1411e9ee
krzyofreak <61771176+krzyofreak@users.noreply.github.com>
2021-07-07 18:09:47 +0530

    Admin product delete complete

--- admin-products.php
+++ admin-products.php
@@ -49,28 +49,28 @@ include_once 'resources/db.php';
             </div>
             </div>
             
-            <button id='c".$row["p_id"]."' type='button' class='btn btn-primary bg-danger' data-toggle='modal' data-target='#exampleModalCenter".$row["p_id"]."'>
+            <button id='c".$row["p_id"]."' type='button' class='btn btn-primary bg-danger' value='".$row["p_id"]."''>
             <i class='fa fa-window-close-o delete px-2 text-light'></i></td>
             </button>
+            <script>
+            $(document).ready(function(){
+              $('#c".$row["p_id"]."').click(function(){
+                if(confirm('Are you sure you want to delete product : ".$row["p_name"]."')){
+                    var val = $('#c".$row["p_id"]."').val();
+                    $.ajax({
+                        type: 'GET',
+                        url: './admin-productsDelete.php',
+                        data: { id: val },
+                        success: function(response) {
+                            alert('product removed');
+                            window.location.href = './products.php';
 
-            <div class='modal fade' id='exampleModalCenter".$row["p_id"]."' tabindex='-1' role='dialog' aria-labelledby='exampleModalCenterTitle' aria-hidden='true'>
-            <div class='modal-dialog modal-dialog-centered' role='document'>
-                <div class='modal-content'>
-                <div class='modal-header'>
-                    <h5 class='modal-title' id='exampleModalLongTitle'>Delete Product Details</h5>
-                    <button type='button' class='close btn btn-secondary bg-danger' data-dismiss='modal' aria-label='Close'>
-                    <span aria-hidden='true'>&times;</span>
-                    </button>
-                </div>
-                <div class='modal-body'>
-                    ".$row["p_id"]."
-                </div>
-                <div class='modal-footer'>
-                    <button type='button' class='btn btn-secondary' data-dismiss='modal'>Close</button>
-                    <button type='button' class='btn btn-primary'>Save changes</button>
-                </div>
-                </div>
-            </div>
+                        }
+                    });
+                }
+              });
+            });
+            </script>
             </div>
         </tr>
     
