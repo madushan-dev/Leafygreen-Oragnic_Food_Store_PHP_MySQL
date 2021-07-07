a6c2795d516ca2dc707de5975b920657bd9c6273
kasuni-upeksha <kasuniupeksha422@gmail.com>
2021-07-07 19:33:19 +0530

    buyer-orders

--- buyer-purchased.php
+++ buyer-purchased.php
@@ -15,10 +15,12 @@
 
                 <div class="list-group">
 
+                    <a href="admin.php"><button type="button" class="list-group-item list-group-item-action ">Dashboard</button></a>
+                    
+                    <a href="orders.php"><button type="button" class="list-group-item list-group-item-action menu-active">Orders</button></a>
+                    
+                    <a href="edit-profile.php"><button type="button" class="list-group-item list-group-item-action">Edit Profile</button></a>
 
-                    <a href="buyer.php"><button type="button" class="list-group-item list-group-item-action ">Dashboard</button></a>
-                    <a href="buyer-purchased.php"><button type="button" class="list-group-item list-group-item-action menu-active">My Purchased</button></a>
-                    <a href="buyer-edit.php"><button type="button" class="list-group-item list-group-item-action">Edit Profile</button></a>
 
                 </div>
 
@@ -27,66 +29,33 @@
 
 
         </div>
+        <script>
+            window.addEventListener("load", function() {
+                $.ajax({
+                    type: "GET", 
+                    url: './buyer-orders.php',
+                    success: function(response) {
+                        $('#orders').html(response);
+                    }
+                });
+            });
+        </script>
         <div class="col-9 dashboard-orders dashboard-table">
 
             <table class="table table-hover order-table ">
                 <thead>
                     <tr>
                         <th scope="col">#</th>
+                        <th scope="col">User</th>
                         <th scope="col">Total Payment</th>
                         <th scope="col">Payment Type</th>
                         <th scope="col">Address</th>
                         <th scope="col">Date</th>
-
+                        <th></th>
                     </tr>
                 </thead>
-                <tbody>
-                    <tr>
-                        <th scope="row">1</th>
-                        <td>Otto</td>
-                        <td>@mdo</td>
-                        <td>@mdo</td>
-                        <td>@mdo</td>
-
-                    </tr>
-                    <tr>
-                        <th scope="row">2</th>
-                        <td>Otto</td>
-                        <td>@mdo</td>
-                        <td>@mdo</td>
-                        <td>@mdo</td>
-
-                    </tr>
-                    <tr>
-                        <th scope="row">3</th>
-                        <td>Otto</td>
-                        <td>@mdo</td>
-                        <td>@mdo</td>
-                        <td>@mdo</td>
-
-                    </tr>
-                    <tr>
-                        <th scope="row">4</th>
-                        <td>Otto</td>
-                        <td>@mdo</td>
-                        <td>@mdo</td>
-                        <td>@mdo</td>
-
-                    </tr>
-                    <tr>
-                        <th scope="row">5</th>
-                        <td>Otto</td>
-                        <td>@mdo</td>
-                        <td>@mdo</td>
-                        <td>@mdo</td>
-
-                    <tr>
-                        <th scope="row">6</th>
-                        <td>Otto</td>
-                        <td>@mdo</td>
-                        <td>@mdo</td>
-                        <td>@mdo</td>
-
+                <tbody id="orders">
+                    
                 </tbody>
             </table>
 
