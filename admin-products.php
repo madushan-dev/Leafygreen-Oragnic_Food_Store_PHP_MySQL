5cbd9f82d07509763f39e0f35a92e511f5711086
krzyofreak <61771176+krzyofreak@users.noreply.github.com>
2021-07-07 14:00:44 +0530

    Update admin-products.php

--- admin-products.php
+++ admin-products.php
@@ -25,11 +25,11 @@ include_once 'resources/db.php';
             <td>@mdo</td>
             <td>".$row["name"]."</td>
             <td class='tools'>
-            <button id='p".$row["p_id"]."' type='button' class='btn btn-primary bg-warning' data-toggle='modal' data-target='#exampleModalCenter'>
+            <button id='p".$row["p_id"]."' type='button' class='btn btn-primary bg-warning' data-toggle='modal' data-target='#exampleModalCenter".$row["p_id"]."'>
             <i class='fa fa-pencil-square-o edit px-2 text-dark'></i>
             </button>
 
-            <div class='modal fade' id='exampleModalCenter' tabindex='-1' role='dialog' aria-labelledby='exampleModalCenterTitle' aria-hidden='true'>
+            <div class='modal fade' id='exampleModalCenter".$row["p_id"]."' tabindex='-1' role='dialog' aria-labelledby='exampleModalCenterTitle' aria-hidden='true'>
             <div class='modal-dialog modal-dialog-centered' role='document'>
                 <div class='modal-content'>
                 <div class='modal-header'>
@@ -39,7 +39,7 @@ include_once 'resources/db.php';
                     </button>
                 </div>
                 <div class='modal-body'>
-                    ...
+                    ".$row["p_id"]."
                 </div>
                 <div class='modal-footer'>
                     <button type='button' class='btn btn-secondary' data-dismiss='modal'>Close</button>
@@ -49,22 +49,21 @@ include_once 'resources/db.php';
             </div>
             </div>
             
-            <button id='p".$row["p_id"]."' type='button' class='btn btn-primary bg-danger' data-toggle='modal' data-target='#exampleModalCenter'>
+            <button id='c".$row["p_id"]."' type='button' class='btn btn-primary bg-danger' data-toggle='modal' data-target='#exampleModalCenter".$row["p_id"]."'>
             <i class='fa fa-window-close-o delete px-2 text-light'></i></td>
             </button>
 
-            <div class='modal fade' id='exampleModalCenter' tabindex='-1' role='dialog' aria-labelledby='exampleModalCenterTitle' aria-hidden='true'>
+            <div class='modal fade' id='exampleModalCenter".$row["p_id"]."' tabindex='-1' role='dialog' aria-labelledby='exampleModalCenterTitle' aria-hidden='true'>
             <div class='modal-dialog modal-dialog-centered' role='document'>
                 <div class='modal-content'>
                 <div class='modal-header'>
-                    <h5 class='modal-title' id='exampleModalLongTitle'>Delete product</h5>
+                    <h5 class='modal-title' id='exampleModalLongTitle'>Delete Product Details</h5>
                     <button type='button' class='close btn btn-secondary bg-danger' data-dismiss='modal' aria-label='Close'>
                     <span aria-hidden='true'>&times;</span>
                     </button>
                 </div>
                 <div class='modal-body'>
-                    Are you sure you want to delete this product?<br>
-                    product name : ".$row["p_name"]."
+                    ".$row["p_id"]."
                 </div>
                 <div class='modal-footer'>
                     <button type='button' class='btn btn-secondary' data-dismiss='modal'>Close</button>
