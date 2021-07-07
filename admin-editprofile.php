140ed8027afbe48d21eea632fd9b7d1bda280210
krzyofreak <61771176+krzyofreak@users.noreply.github.com>
2021-07-08 01:05:04 +0530

    Update admin-editprofile.php
    validation

--- admin-editprofile.php
+++ admin-editprofile.php
@@ -18,68 +18,33 @@ include_once 'resources/db.php';
     $result = $stmt->get_result();
 
     if(mysqli_num_rows($result) == 1) {
-        if($_POST["name"] != null){
-            $sql = "UPDATE users SET name = ? WHERE u_id = ?";
-            $stmt = $conn->prepare($sql);
-            $stmt->bind_param("ss",$name, $uid);
-            $name = $_POST["name"];
-            $uid = $_SESSION["userID"];
-            if ($stmt->execute() === TRUE) {
-                echo "Name updated";
-            }
-        }else {
-            echo "empty";
-        }
-
-        if($_POST["email"] != null){
-            $sql = "UPDATE users SET email = ? WHERE u_id = ?";
-            $stmt = $conn->prepare($sql);
-            $stmt->bind_param("ss",$email, $uid);
-            $email = $_POST["email"];
-            $uid = $_SESSION["userID"];
-            if ($stmt->execute() === TRUE) {
-                echo "email updated";
-            }
-        }else {
-            echo "empty";
-        }
-
-        if($_POST["password"] != null){
-            $sql = "UPDATE users SET password = ? WHERE u_id = ?";
-            $stmt = $conn->prepare($sql);
-            $stmt->bind_param("ss",$password, $uid);
-            $password = $_POST["password"];
-            $uid = $_SESSION["userID"];
-            if ($stmt->execute() === TRUE) {
-                echo "password updated";
-            }
-        }else {
-            echo "empty";
-        }
-
-        if($_POST["phone"] != null){
-            if (strlen($_POST["phone"]) == 10){
-                if(is_numeric($_POST["phone"])){
-                    $sql = "UPDATE users SET phone_number = ? WHERE u_id = ?";
-                    $stmt = $conn->prepare($sql);
-                    $stmt->bind_param("ss",$phone, $uid);
-                    $phone = $_POST["phone"];
-                    $uid = $_SESSION["userID"];
-                    if ($stmt->execute() === TRUE) {
-                        echo "phone number updated";
+        if($_POST["name"] != null || $_POST["email"] != null || $_POST["phone"] != null || $_POST["password"] != null){
+            if(strlen($_POST["password"]) > 8){
+                if(filter_var($_POST["email"], FILTER_VALIDATE_EMAIL)){
+                    if(is_numeric($_POST["phone"]) || strlen($_POST["phone"]) == 10){
+                        $sql = "UPDATE users SET name = ? , email = ? , password = ? , phone_number = ? WHERE u_id = ?";
+                        $stmt = $conn->prepare($sql);
+                        $stmt->bind_param("sssss", $name, $email, $password, $email, $uid);
+                        $name = $_POST["name"];
+                        $email = $_POST["email"];
+                        $phone = $_POST["phone"];
+                        $password = $_POST["password"];
+                        $uid = $_SESSION["userID"];
+                        if ($stmt->execute() === TRUE) {
+                            echo "Details updated";
+                        }
+                    }else{
+                        echo "invalid phone number";
                     }
                 }else{
-                    echo "not a valid phone number";
+                    echo "invalid email";
                 }
-               
             }else{
-                echo "not a valid phone number";
+                echo "Weak Password";
             }
-            
         }else {
-            echo "empty";
+            echo "Fields are empty";
         }
-
 
     }else{
         echo "error";
